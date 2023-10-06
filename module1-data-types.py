d4e8275a651e1e92cf8a9a9225eb93849cf2fd39
Kristinimum <kristinmaxwell43@gmail.com>
2023-10-06 14:23:06 +0000

    operators

---
+++ module1-data-types.py
@@ -0,0 +1,11 @@
+greeting = 'Hello, friend!'
+age = 35
+speed = 4.5
+speed1 = 4.0
+speed2 = 4.
+speed3 = 4
+am_i_ugly = True
+print(age) # here is a midline comment
+# here is an all line comment
+print(greeting)
+print(speed2)
---
+++ module1-operators.py
@@ -0,0 +1,13 @@
+age = 28
+print(age)
+new_age = age + 5
+print(new_age)
+age += 7
+print(age)
+age *= 2
+age -= 5
+age /+ 3
+text = 'hokus' + 'pokus'
+print(text)
+print('hokus' * 5)
+print(23+5)
