cf5772daa01629ed12c36c710d09f088bf883d1c
Kristinimum <kristinmaxwell43@gmail.com>
2023-10-09 22:01:21 +0000

    wednesdaymodule-and-exercisefromHR

---
+++ mod4-intro-dictionaries.py
@@ -0,0 +1,15 @@
+#dictionaries store key value pairs
+emails = {
+    'Anne Stahl': 'astahl@gmail.com',
+    'Peter Small': 'peters@yandex.com',
+    'Mark Steel': 'mark@steel.com'
+}
+print(emails['Mark Steel'])
+
+spanish_animals = {
+    'dog': 'el perro',
+    'cat': 'el gato',
+    'horse': 'el caballo',
+    'bird': 'el pájaro'
+     }
+print(spanish_animals['bird'])
