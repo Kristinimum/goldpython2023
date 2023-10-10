ce797800911f936ca96d870d63833ffa239e0c63
Kristinimum <kristinmaxwell43@gmail.com>
2023-10-10 11:42:41 +0000

    thursdaymods-and-exer10

---
+++ mod5intro-functions.py
@@ -0,0 +1,6 @@
+#helpful to have well-defined and named functions will make it easier
+#good practice to keep all definitions at the top of your file.
+def greet():
+    print('Hello, my dear!')
+greet()
+
